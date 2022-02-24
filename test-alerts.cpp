35f66c5e81a27bee00c4bdefdda9ecdfde88c18a
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-02-25 00:11:56 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -26,28 +26,35 @@ TEST_CASE("classify the temperature breach according to limits") {
 TEST_CASE("checks for the breach and and sends alerts") {
 	BatteryCharacter testChar;
 	testChar.coolingType = PASSIVE_COOLING;
-	checkAndAlert(TO_CONTROLLER, testChar, 10);
+	REQUIRE(checkAndAlert(TO_CONTROLLER, testChar, 10) == ALERT_SENT);
 	testChar.coolingType = HI_ACTIVE_COOLING;
-	checkAndAlert(TO_CONTROLLER, testChar, -5);
+	REQUIRE(checkAndAlert(TO_CONTROLLER, testChar, -5) == ALERT_SENT);
 	testChar.coolingType = MED_ACTIVE_COOLING;
-	checkAndAlert(TO_CONTROLLER, testChar, 45);
+	REQUIRE(checkAndAlert(TO_CONTROLLER, testChar, 45) == ALERT_SENT);
 
 	testChar.coolingType = PASSIVE_COOLING;
-	checkAndAlert(TO_EMAIL, testChar, 10);
+	REQUIRE(checkAndAlert(TO_EMAIL, testChar, 50) == ALERT_SENT);
 	testChar.coolingType = HI_ACTIVE_COOLING;
-	checkAndAlert(TO_EMAIL, testChar, -5);
+	REQUIRE(checkAndAlert(TO_EMAIL, testChar, -5) == ALERT_SENT);
 	testChar.coolingType = MED_ACTIVE_COOLING;
-	checkAndAlert(TO_EMAIL, testChar, 45);
+	REQUIRE(checkAndAlert(TO_EMAIL, testChar, 45) == ALERT_SENT);
+
+	testChar.coolingType = PASSIVE_COOLING;
+	REQUIRE(checkAndAlert(TO_EMAIL, testChar, 10) == ALERT_NOT_REQUIRED);
+	testChar.coolingType = HI_ACTIVE_COOLING;
+	REQUIRE(checkAndAlert(TO_EMAIL, testChar, 40) == ALERT_NOT_REQUIRED);
+	testChar.coolingType = MED_ACTIVE_COOLING;
+	REQUIRE(checkAndAlert(TO_EMAIL, testChar, 30) == ALERT_NOT_REQUIRED);
 }
 
 TEST_CASE("Sends breach type message to controller when temperature breaches limits") {
-	sendToController(NORMAL);
-	sendToController(TOO_LOW);
-	sendToController(TOO_HIGH);
+	REQUIRE(sendToController(NORMAL) == ALERT_SENT);
+	REQUIRE(sendToController(TOO_LOW) == ALERT_SENT);
+	REQUIRE(sendToController(TOO_HIGH) == ALERT_SENT);
 }
 
 TEST_CASE("Sends breach type message to email when temperature breaches limits") {
-	sendToEmail(NORMAL);
-	sendToEmail(TOO_LOW);
-	sendToEmail(TOO_HIGH);
+	REQUIRE(sendToEmail(NORMAL) == ALERT_NOT_REQUIRED);
+	REQUIRE(sendToEmail(TOO_LOW) == ALERT_SENT);
+	REQUIRE(sendToEmail(TOO_HIGH) == ALERT_SENT);
 }
